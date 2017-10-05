bc05e58b2e54e2f85795181795f0eae6168b9efb
Sean Thames <secris1@gmail.com>
2017-10-05 09:52:59 -0400

    Function to perform a salted SHA256 hash in PowerShell

---
+++ Powershell/Sha256-Hashing.ps1
@@ -0,0 +1,8 @@
+﻿Function Get-Sha256([String] $String1, [String] $String2) 
+{ 
+    $StringBuilder = New-Object System.Text.StringBuilder 
+    [System.Security.Cryptography.HashAlgorithm]::Create("SHA256").ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String1 + $String2))|%{ 
+        [Void]$StringBuilder.Append($_.ToString("x2")) 
+    } 
+    $StringBuilder.ToString() 
+}
